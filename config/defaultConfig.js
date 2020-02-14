747d015bcc7b7d00f464b486954733bee1f14115
rotemyohanan <rotemyo@icloud.com>
2020-02-14 06:34:26 +0200

    add config

---
+++ config/defaultConfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "Customer": {
+      "dbConfig": {
+        "host": "localhost",
+        "port": 5984,
+        "dbName": "customers"
+      },
+      "credit": {
+        "initialLimit": 100,
+        // Set low for development
+        "initialDays": 1
+      }
+    }
+  }
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,6 +7,11 @@ router.get('/', async (req, res) => {
   return res.status(200).send('A new main request succeeded')
 })
 
+router.get('/health-check', async (req, res) => {
+  console.log('successful health-check')
+  return res.status(200).send('health-check ok')
+})
+
 router.post('/flights', async (req, res) => {
   const name = req.body.name
   console.log(`A new flight with name: ${name}, was recorded.`)
@@ -14,5 +19,4 @@ router.post('/flights', async (req, res) => {
   return res.status(201).send(`A new flight name: ${newflight.name} was created in the database: ${newflight}`)
 })
 
-// console/log()
 module.exports = router

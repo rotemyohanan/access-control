8f962f009c4b60f295d66e7eba9d7462af3dfd59
Rotemy <RotemYo@icloud.com>
2020-02-14 03:03:25 +0200

    base files

---
+++ README.md
@@ -0,0 +1,40 @@
+# access
+This is the core code to ..
+This is the main knowledge base.
+
+### Getting started
+
+In order to run the project please run the following ```
+```javascript
+
+```
+
+
+### components
+
+There are 3 main features to be set in every computer
+1. MongoDB Compass
+2. Postman
+3. AWS-S3 account
+
+
+### Access
+Currently the data can be accessed only under the igentify company VPN.
+
+
+### Installation
+...
+
+
+### Running the tests
+
+The project contains 3 different type of tests
+
+### Deployment
+The project contains 3 different type of tests
+
+### Author
+The project is written by Rotem Yohanan.
+
+### Sources
+The project contains data from external sources reached by public APIs
---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+//const positiveControlSchema = require('../data/models/positivecontrol').positiveControlSchema
+//const PositiveControl = require('../data/models/positivecontrol').PositiveControl
+//const positiveControlService = require('../data/services/positiveControlService')
+const config = require('config')
+const flightRouter = require('./routes/flightRoutes')
+const logger = require('./middlewares/logger')
+const startupDebugger = require('debug')('app:startup')
+const dbDebugger = require('debug')('app:db')
+
+// mongoose.connect('mongodb://127.0.0.1/airwits')
+// .then( () => console.log('Connected to MongoDB ...'))
+// .catch(err => console.error('Could not connect to MongoDB ...', err))
+
+app.use(express.json())
+app.use('/api/v1', flightRouter)
+
+//console.log(`app host: ${config.get("website.host")}`)
+startupDebugger('Main application is running')
+//console.log(`Mail password: ` + config.get('mail.password'))
+dbDebugger('Connected to the database library ____ ')
+
+/*app.get("/api/v1/positive-controls/sampleId/:sampleId/variant-name/:name", async (req, res) => {
+    let variantName = req.params.name
+    debugger
+    const result = await PositiveControl.find({name : variantName})
+    if(!result) {
+        res.status(404).send(`Positive control name: ${variantName} does not exist in the database`)
+    }
+    res.status(200).send(`Positive control name: ${variantName} was found in the database` )
+})
+
+app.post("/api/v1/positive-controls", async (req, res) => {
+    let fullName = `${req.body.gene}_${req.body.mutation}`
+    console.log("Full name : " + fullName)
+    const existingPositiveControl = await PositiveControl.find({name: fullName})
+    console.log(existingPositiveControl)
+    if(existingPositiveControl.length >0) {
+        return res.status(409).send(`Positive control name: ${fullName} already exists in the database`)
+    }
+    const positiveControl = positiveControlService.createNewPositiveControl(req)
+    const result = await positiveControl.save()
+    return res.status(201).send(`A new positive control name: ${result.name} was created in the database: ${result}`)
+}) 
+*/
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`The application is listenning on port ${port}`))
+
